c1180bf5bd0617145dfc9b02fc90c4eccacaf482
Maxime GELLE <maxime.gelle@gmail.com>
2019-10-27 18:35:21 +0100

    Add Provider contract verification

--- catalog/build.gradle.kts
+++ catalog/build.gradle.kts
@@ -3,8 +3,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.2.0.RELEASE"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
+	id("au.com.dius.pact") version "4.0.2"
 	kotlin("jvm") version "1.3.50"
 	kotlin("plugin.spring") version "1.3.50"
+}
+
+pact {
+	serviceProviders {
+		create("Catalog_Provider") {
+			hasPactsFromPactBroker(mapOf("authentication" to listOf("Bearer", "1gX52gsVt9iwwVuD9ciMaA")), "https://magelle.pact.dius.com.au")
+		}
+	}
 }
 
 group = "contract-testing"
@@ -23,10 +32,12 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
+	testImplementation("au.com.dius:pact-jvm-provider-junit5_2.12:3.6.14")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	systemProperty("pact.rootDir", "build/pacts")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ catalog/src/test/kotlin/contracttesting/catalog/CatalogContractTest.kt
@@ -0,0 +1,39 @@
+package contracttesting.catalog
+
+import au.com.dius.pact.provider.junit.Provider
+import au.com.dius.pact.provider.junit.loader.PactBroker
+import au.com.dius.pact.provider.junit.loader.PactBrokerAuth
+import au.com.dius.pact.provider.junit5.HttpTestTarget
+import au.com.dius.pact.provider.junit5.PactVerificationContext
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.TestTemplate
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.net.URL
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(classes = [CatalogApplication::class],
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+        properties = ["server.port=8080"])
+@Provider("Catalog_Provider")
+@PactBroker(scheme = "https", host = "magelle.pact.dius.com.au", port="443",
+        authentication = PactBrokerAuth(scheme = "Bearer", username = "1gX52gsVt9iwwVuD9ciMaA", password = "")
+)
+class CatalogContractTest {
+
+    private val providerUrl = "http://localhost:8080"
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider::class)
+    fun pactVerificationTestTemplate(context: PactVerificationContext) {
+        context.verifyInteraction()
+    }
+
+    @BeforeEach
+    @Throws(Exception::class)
+    fun before(context: PactVerificationContext) {
+        context.target = HttpTestTarget.fromUrl(URL(providerUrl))
+    }
+}
